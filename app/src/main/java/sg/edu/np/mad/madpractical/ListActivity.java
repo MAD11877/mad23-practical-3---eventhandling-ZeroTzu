caff2c85ae6412148c4dec532affa7fdae8d00f0
ZeroTzu <chingheng922@gmail.com>
2023-05-11 11:31:24 +0800

    practical 3 complete(Not including Challenge)

---
+++ app/src/main/java/sg/edu/np/mad/madpractical/ListActivity.java
@@ -0,0 +1,57 @@
+package sg.edu.np.mad.madpractical;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+import java.util.Random;
+
+public class ListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        ImageView logoImage = findViewById(R.id.logoImage1);
+        logoImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListActivity.this);
+                builder.setTitle("Profile");
+                builder.setMessage("MADness");
+                builder.setCancelable(false);
+                builder.setPositiveButton("View", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Random rand= new Random();
+                        int randomInt=rand.nextInt(100000);
+                        String text="MAD "+randomInt;
+                        Intent activity =  new Intent(ListActivity.this,MainActivity.class);
+                        activity.putExtra("string1",text);
+                        startActivity(activity);
+
+                    }
+                });
+                builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+    }
+}
--- app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
@@ -4,54 +4,57 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean isFollowing = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        boolean isFollowing = false;
         ImageView logoImage = findViewById(R.id.logoImage);
         Button followButton = findViewById(R.id.btnFollow);
-        logoImage.setOnClickListener(new View.OnClickListener() {
+        followButton.setText("Follow");
+        followButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Profile");
-                builder.setMessage("MADness");
-                builder.setCancelable(false);
-                builder.setPositiveButton("View", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Random rand= new Random();
-                        int randomInt=rand.nextInt(100000);
-                        String text="MAD "+randomInt;
-                        TextView tv2= findViewById(R.id.textView2);
-                        tv2.setText(text);
+                if (isFollowing==false){
+                    isFollowing=true;
+                    followButton.setText("Unfollow");
+                    Toast.makeText(getApplicationContext(),"following",Toast.LENGTH_SHORT).show();
 
-                    }
-                });
-                builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
 
-                AlertDialog alertDialog = builder.create();
-                alertDialog.show();
+
+                }
+                else {
+                    isFollowing=false;
+                    followButton.setText("Follow");
+                    Toast.makeText(getApplicationContext(),"stopped following",Toast.LENGTH_SHORT).show();
+
+
+                }
+
             }
         });
+
+
+
+        Intent recieveIntent= getIntent();
+        String message=recieveIntent.getStringExtra("string1");
+        TextView tv = findViewById(R.id.textView2);
+        tv.setText(message);
+
     }
 }
 
